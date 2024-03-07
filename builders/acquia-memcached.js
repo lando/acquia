0fb576ddf81aed8ca617b26ba907de3c71660882
Alec Reynolds <alec+git@lando.dev>
2024-03-07 13:44:36 -0800

    Improve database selection. (#85)
    * Improve database selection.
    
    * Remove laravel references.
    
    * Remove laravel references.
    
    * Correct paths to downstreamer tests.
    
    * Improve database selection logic.
    
    * Update downstreamer to php 8.1.

--- builders/acquia-memcached.js
+++ builders/acquia-memcached.js
@@ -5,9 +5,9 @@ const LandoMemcached = require('@lando/memcached/builders/memcached.js');
 
 // Builder
 module.exports = {
-  name: 'laravel-memcached',
+  name: 'acquia-memcached',
   parent: '_service',
-  builder: (parent, config) => class LaravelMemcached extends LandoMemcached.builder(parent, LandoMemcached.config) {
+  builder: (parent, config) => class AcquiaMemcached extends LandoMemcached.builder(parent, LandoMemcached.config) {
     constructor(id, options = {}) {
       super(id, options, {services: _.set({}, options.name)});
     };
